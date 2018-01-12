a737ad4264f5b2cbcd795531f64dfef90e83e9c8
Martin Betz <martinmichaelbetz@gmail.com>
2018-01-12 07:25:46 +0100

    Update: 180112-01

---
+++ source/_components/img.blade.php
@@ -0,0 +1,3 @@
+<img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+  class="lazyload mb-4"
+  data-srcset="{{ $src }}" alt="{{ $alt }}" />
---
+++ source/stimmen.blade.php
@@ -0,0 +1,64 @@
+@extends('_layouts.master')
+
+@section('body')
+<main class="p-8 bg-green">
+
+    <div class="text-3xl mb-4 text-white">Stimmen zu Code+Design</div>
+
+   <div class="p-8 bg-white">
+
+    
+       
+    <div class="flex flex-wrap">
+        @foreach ($testimonials as $testimonial)
+                <div class="w-full md:w-1/2 p-1">@component('_components.card')
+                             
+                                             @slot('src')
+                                             {{ $testimonial->src }}
+                                             @endslot
+                             
+                                             @slot('link')
+                                             {{ $testimonial->resource }}
+                                             @endslot
+                             
+                                             @slot('medium')
+                                             {{ $testimonial->medium }}
+                                             @endslot
+                             
+                                             @slot('channel')
+                                            {{ $testimonial->channel }}
+                                             @endslot
+                             
+                                             @slot('published_at')
+                                             {{ date('d.m.y', $testimonial->published_at) }}
+                                             @endslot
+                             
+                                             @slot('ml')
+                                             @endslot
+                             
+                                             @slot('mr')
+                                             @endslot
+
+                                             @slot('m')
+                                             @endslot
+
+                                             @slot('mb')
+                                             @endslot
+                             
+                                             {{ $testimonial->content }}
+                             
+                                          @endcomponent</div>         
+    
+        @endforeach
+    </div>
+
+
+
+
+    </div>
+
+   </div>
+
+</main>
+
+@endsection
