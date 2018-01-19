9ece8f250126bc6f1db515c183df6abeb5584145
Martin Betz <martinmichaelbetz@gmail.com>
2018-01-19 08:43:51 +0100

    Add: 404

---
+++ source/404.html.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('body')
+
+<div class="p-4 bg-green">
+    <div class="bg-white p-4">
+        <p class="text-2xl">Das hat leider nicht funktioniert.</p> 
+        <p>Infos zu den Camps gibt es <a href="/camps">hier</a>,
+        bist du dir sicher, dass du einen kaputten Link entdeckt hast, freuen wir uns über deine <a href="/kontakt">Nachricht</a>.
+        </p>
+    </div>
+</div>
+
+@endsection
