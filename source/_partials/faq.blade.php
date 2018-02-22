65400f83ec607ec5287f318d654a38c0f760e2bf
TSRed <36697087+TSRed@users.noreply.github.com>
2018-02-22 15:51:57 +0100

    add a question and answer to camp timetable

--- source/_partials/faq.blade.php
+++ source/_partials/faq.blade.php
@@ -14,6 +14,9 @@
         <dt class="font-bold">Wie laut wird es auf dem Camp?</dt>
         <dd class="mb-4">Gehe besser davon aus, dass es auf dem Camp laut und hektisch wird. Bringe dir Kopfhörer mit, über die du Musik, <a href="https://calmyleon.com" target="_blank">beruhigende Geräusche</a> oder <a href="https://mynoise.net/NoiseMachines/whiteNoiseGenerator.php" target="_blank">Rauschen</a>  hörst. Bei manchen Camps haben wir in der Location auch ruhigere Ecken. Das können wir aber nicht garantieren.</dd>
         
+          <dt class="font-bold">Hat das Camp einen festen Zeitplan?</dt>
+        <dd class="mb-4"> "Wenn ihr einen klaren Ablauf gewohnt seid, dann seid ihr hier falsch. Bei dem Camp haben wir die Möglichkeit selber unseren Zeitplan zu bestimmen, dabei war es umso besser, dass uns erfahrene Coaches halfen. Ich selber habe viele Erfahrungen anderer Teilnehmer mitgenommen.".</dd>
+        
     </dl>
 
 </div>
