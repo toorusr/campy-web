0aa15763fa2851b5de790a0bf83c94003257f7b4
Martin Betz <martinmichaelbetz@gmail.com>
2018-02-24 15:14:53 +0100

    Add: Confirmation page

---
+++ source/newsletter/bestaetigen.blade.php
@@ -0,0 +1,20 @@
+@extends('_layouts.master')
+
+@section('body')
+<main class="p-8 bg-red">
+
+    <div class="bg-white p-8">
+        <div class="text-3xl">
+            Du hast eine Mail von uns bekommen. <br>
+            Klicke bitte den Bestätigungslink darin. <br>
+            Dann bekommst du unseren Newsletter!
+        </div>
+    </div>
+
+</main>
+
+@endsection
+
+@section('title')
+Newsletter
+@endsection
