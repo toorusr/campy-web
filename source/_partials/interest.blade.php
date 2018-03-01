4be04f550a9cc2ccd191ab001c0957af2f6c6021
Martin Betz <martinmichaelbetz@gmail.com>
2018-03-01 16:28:10 +0100

    Fix: Interest comp button

--- source/_partials/interest.blade.php
+++ source/_partials/interest.blade.php
@@ -2,19 +2,19 @@
     <h3 class="mb-4">Interesse dabeizusein?</h3>
 <form class="w-full">
   <div class="flex flex-wrap -mx-3 mb-2">
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+    <div class="flex-1 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-city">
         Vorname
       </label>
       <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="firstname" type="text" placeholder="Simone">
     </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+    <div class="flex-1 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-zip">
         Email
       </label>
       <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="email" type="email" placeholder="robot@simonegiertz.com">
     </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+    <div class="flex-1 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="city">
         Stadt
       </label>
@@ -35,6 +35,9 @@
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
       </div>
+    </div>
+    <div class="self-end">
+       <button class="px-3 mb-6 md:mb-0  py-3 text-grey-darkest mr-4 rounded bg-grey  hover:bg-grey-dark">Senden</button> 
     </div>
   </div>
 </form></div>
