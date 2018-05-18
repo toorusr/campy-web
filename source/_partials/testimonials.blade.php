4a856f0f0d88d26f3f81b387e05005010b183862
Toorusr <toorusr@protonmail.com>
2018-05-18 22:59:26 +0200

    Add: dynamic testimonials

---
+++ source/_partials/testimonials.blade.php
@@ -0,0 +1,30 @@
+<div class="mb-4 mt-4">
+    <div class="text-3xl mb-4 mt-8 ml-4"><a href="/initiative/stimmen" class="no-underline border-b-2 border-purple text-grey-darkest">Die Stimmen</a></div>
+    <div class="md:flex">
+      @php
+          $testimonials->splice(3)
+      @endphp
+      @foreach ($testimonials as $testimonial)
+          <div>
+              @component('_components.card')
+                  @slot('src')
+                      {{$testimonial->src}}
+                  @endslot
+                  @slot('link')
+                      {{$testimonial->resource}}
+                  @endslot
+                  @slot('medium')
+                      {{$testimonial->medium}}
+                  @endslot
+                  @slot('channel')
+                      {{$testimonial->channel}}
+                  @endslot
+                  @slot('published_at')
+                      {{ date('d.m.y', $testimonial->published_at) }}
+                  @endslot
+                  {{$testimonial->content}}
+              @endcomponent
+          </div>
+      @endforeach
+    </div>
+</div>
