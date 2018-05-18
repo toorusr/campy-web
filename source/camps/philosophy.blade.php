6b39bad960e77f65c5e922ecc6117a1a25dead93
Toorusr <toorusr@protonmail.com>
2018-05-18 16:52:10 +0200

    Add: philosophy page

---
+++ source/camps/philosophy.blade.php
@@ -0,0 +1,18 @@
+@extends('_layouts.master')
+
+@section('body')
+<main class="p-8 bg-green">
+    <div class="text-3xl mb-4 text-white">Philosophy der Camps</div>
+        <div class="p-8 rounded bg-white">
+
+        # Test
+
+        </div>
+    </div>
+</main>
+
+@endsection
+
+@section('title')
+Stimmen
+@endsection
