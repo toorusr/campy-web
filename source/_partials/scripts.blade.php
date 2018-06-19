757ef95f9100b9c5b5bd3eb4e35dcd52b4c62fe9
Toorusr <toorusr@protonmail.com>
2018-06-19 23:31:22 +0200

    Cut: google analytics

--- source/_partials/scripts.blade.php
+++ source/_partials/scripts.blade.php
@@ -1,17 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-75151815-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-75151815-2');
-</script>
-<!-- End: Google Analytics -->
-
 <!-- Start: Hubspot Forms -->
 <script src="//js.hsforms.net/forms/v2.js"></script>
 <!-- End: Hubspot Forms -->
-
-
-
