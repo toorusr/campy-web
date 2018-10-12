8543f5e9bcf9b8c681c3e1c816d75d4ee0150d0b
Martin Betz <martinmichaelbetz@gmail.com>
2018-10-12 18:05:29 +0200

    Fix: img component

--- source/_components/img.blade.php
+++ source/_components/img.blade.php
@@ -1,3 +1,3 @@
 <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-  class="lazyload mb-4 {{ $height or '' }}{{ $width or '' }} {{ $rounded or '' }}"
-  data-srcset="{{ $src }}" alt="{{ $alt or '' }}" style="{{ $style or '' }}"/>
+  class="lazyload mb-4 {{ $height or '' }} {{ $width or '' }} {{ $rounded or '' }}"
+  data-srcset="{{ $src }}" alt="{{ $alt }}" />
