20d09fb9813f3b574b31c7ef2d60a080773b69c5
Martin Betz <martinmichaelbetz@gmail.com>
2018-11-30 19:10:38 +0100

    Fix: Magazine order add email

--- source/magazin/bestellen.blade.php
+++ source/magazin/bestellen.blade.php
@@ -13,18 +13,24 @@
         
 <div class="rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
     <div class="-mx-3 md:flex mb-6">
-        <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+        <div class="md:w-1/3 px-3 mb-6 md:mb-0">
             <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
                 Vorname
             </label>
             <input class="appearance-none block w-full bg-grey-lighter text-grey-darker rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Grace" name="vorname" requipurple>
             <input type="text" name="hene" hidden>
         </div>
-        <div class="md:w-1/2 px-3">
+        <div class="md:w-1/3 px-3">
             <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-last-name" requipurple>
                 Nachname
             </label>
             <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text" placeholder="Hopper" name="nachname">
+        </div>
+        <div class="md:w-1/3 px-3">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-email" requipurple>
+                E-Mail
+            </label>
+            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="email" placeholder="grace@hopper.net" name="email">
         </div>
     </div>
     <div class="-mx-3 md:flex mb-6">
