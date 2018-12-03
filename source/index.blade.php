3168e7c61f203bcf20875067f406d1fc79a28409
Martin Betz <martinmichaelbetz@gmail.com>
2018-12-03 11:42:25 +0100

    Fix: More general description for events

--- source/index.blade.php
+++ source/index.blade.php
@@ -22,7 +22,7 @@
             <div class="mb-4 lg:ml-8 md:flex flex-col justify-between lg:w-1/3">
                 <div>
                     <p class="text-3xl mb-4 "><a href="/initiative" class="no-underline text-grey-darkest"><span class="border-b-2 border-purple">Die Initiative</span></a></p>
-                    <p class="leading-normal hyphens">Die gemeinnützige Code+Design Initiative hat sich zum Ziel gesetzt, Jugendliche für digitale Technologien und Berufe zu begeistern und insbesondere den Anteil an Frauen in diesen Bereichen zu erhöhen. Dafür haben wir im letzten Jahr 8 Code+Design Camps in 6 verschiedenen Städten mit über 650 Teilnehmern organisiert. Dieses Jahr wollen wir noch mehr Jugendlichen die Möglichkeit geben eines unserer Events zu besuchen. Deshalb arbeiten wir an weiteren Formaten um die Zeit zwischen den Code+Design Camps zu verkürzen und unsere Reichweite zu vergrößern.</p>
+                    <p class="leading-normal hyphens">Die gemeinnützige Code+Design Initiative hat sich zum Ziel gesetzt, Jugendliche für digitale Technologien und Berufe zu begeistern und insbesondere den Anteil an Frauen in diesen Bereichen zu erhöhen. Dafür veranstalten wir deutschlandweit Events – von eintägigen Meetups bis zu mehrtägigen Camps.</p>
                 </div>
                 @component('_components.button') @slot('colorback') purple @endslot @slot('colorfront') white @endslot @slot('link') initiative @endslot @slot('width') w-64 @endslot Zur Initiative @endcomponent
             </div>
