1113d64b7ae046ae48074690794b8800b131d25a
Martin Betz <martinmichaelbetz@gmail.com>
2018-12-03 12:55:15 +0100

    Add: Description and download for magazine 2

--- source/magazin.blade.php
+++ source/magazin.blade.php
@@ -50,6 +50,40 @@
                     @endslot
                     Kostenlos bestellen
                 @endcomponent
+            </div>
+            <div class="mt-4">
+                <div class="text-2xl font-bold">2016/2017 - Ausgabe 1</div>
+                <div class="text-lg">
+                    Hallo, Computer: Mensch und Maschine verstehen sich immer besser + Big Data, DIY-Spracherkennung, Welcher IT-Typ bin ich?
+                </div>
+            </div>
+            <div class="mt-4">
+                @component('_components.button')
+                    @slot('link')
+                        files/code-design-magazine-001.pdf
+                    @endslot
+                    @slot('colorback')
+                        purple
+                    @endslot
+                    @slot('colorfront')
+                        white
+                    @endslot
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg> Download
+                @endcomponent
+                @component('_components.button')
+                    @slot('colorback')
+                        purple
+                    @endslot
+                    @slot('colorfront')
+                        white
+                    @endslot
+                    @slot('link')
+                        magazin/bestellen
+                    @endslot
+                    @slot('width')
+                    @endslot
+                    Kostenlos bestellen
+                @endcomponent
                 {{-- <a href="/{{ $link }}" class="no-underline text-black inline-block text-2xl px-4 py-2 leading-none border rounded border-black hover:border-transparent hover:bg-{{ $colorback }} hover:text-{{ $colorfront }} mt-4 lg:mt-0">{{ $slot }}</a> --}}
             </div>
         </div>
