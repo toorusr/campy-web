fcc38fa92c3c6d82e6b7199cde2bad975f7e2cf9
Martin Betz <martinmichaelbetz@gmail.com>
2018-12-06 14:25:23 +0100

    Add: Film page

---
+++ source/magazin/filme.blade.php
@@ -0,0 +1,15 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="p-4 bg-purple">
+    <div class="bg-white p-4">
+        <p class="text-2xl mb-4">Filme</p>
+        <p>Welche Hacker-Filme fallen dir noch an? Welche Serien mit Tech-Thematik gefallen dir?</p>
+        <p><a href="mailto:hello@code.design?subject=Hacker-Filme">Schreib uns</a>, dann schreiben wir darüber</p>
+    </div>
+</div>
+@endsection
+
+@section('title')
+Filme
+@endsection
