7653e54e8e3eec5669a6a4be2d419b2c592f942c
Martin Betz <martinmichaelbetz@gmail.com>
2019-01-14 13:22:48 +0100

    Add: Community pages

---
+++ source/community/anmelden.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('body')
+
+<div class="p-4 bg-purple">
+    <div class="text-white text-3xl mb-4">Werde Teil der Code+Design Community</div>
+        <div class="mt-4"><script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrLY2LjDtwlufoJD?backgroundColor=purple" frameborder="0" onmousewheel="" width="100%" height="1525.003906" style="background: transparent; border: 1px solid #ccc;"></iframe></div>
+</div>
+    <div class="bg-white p-4">
+
+    </div>
+</div>
+
+@endsection
