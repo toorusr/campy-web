5cb652493879d6d96978383d1b34f59143f546c3
Julius <jubeki99@gmail.com>
2019-01-15 17:58:07 +0100

    Fix formatting _partials.timetable

--- source/_partials/timetable.blade.php
+++ source/_partials/timetable.blade.php
@@ -1,273 +1,228 @@
-<div class="mt-8 text-2xl font-bold">Was auf einem Code+Design Camp passiert</div>
+<div class="mt-8 text-2xl font-bold">
+    Was auf einem Code+Design Camp passiert
+</div>
 
 <ul class="mt-2 leading-normal">
-  <li>Kreativübungen</li>
-  <li>Ideen finden und entwickeln</li>
-  <li>Pitch-Training und Ideen-Pitch</li>
-  <li>Teamarbeit</li>
-  <li>Workshops</li>
-  <li>Impulsvorträge</li>
-  <li>Präsentationen</li>
-  <li>Viele gleichgesinnte Jugendliche, die etwas bewegen wollen!</li>
-  <li>Kontakte mit erfahrenen Praktikern und Firmen</li>
+    <li>Kreativübungen</li>
+    <li>Ideen finden und entwickeln</li>
+    <li>Pitch-Training und Ideen-Pitch</li>
+    <li>Teamarbeit</li>
+    <li>Workshops</li>
+    <li>Impulsvorträge</li>
+    <li>Präsentationen</li>
+    <li>Viele gleichgesinnte Jugendliche, die etwas bewegen wollen!</li>
+    <li>Kontakte mit erfahrenen Praktikern und Firmen</li>
 </ul>
 
 {{-- <div class="mt-8 text-2xl font-bold">Ablauf</div>
 <div>(Nur zur Orientierung, am Beispiel eines 4-Tages-Camps. Weicht ab!) </div>
 <div class="mt-8 border-2 rounded p-4 lg:flex lg:flex-wrap">
     <div id="thursday" class={{ $width }} p-4">
-      <p class="font-bold mb-4 md:text-center text-xl">Tag 1</p>
-      <table>
-        <tr>
-          <td class="p-1"><span class="font-bold">09:00</span>
-          </td>
-          <td class="p-1">Ankommen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">10:00</span>
-          </td>
-          <td class="p-1">Kennenlernen & Erwartungen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">11:00</span>
-          </td>
-          <td class="p-1">Kreativübungen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">12:00</span>
-          </td>
-          <td class="p-1">Ideen finden und entwickeln</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">13:00</span>
-          </td>
-          <td class="p-1">Mittagessen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">14:00</span>
-          </td>
-          <td class="p-1">Pitch-Training und Ideen-Pitch</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">15:00</span>
-          </td>
-          <td class="p-1">Teamfindung</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">16:00</span>
-          </td>
-          <td class="p-1">Projekt-Kickoff</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">17:00</span>
-          </td>
-          <td class="p-1">Projektarbeit</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">18:00</span>
-          </td>
-          <td class="p-1">Abendessen</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">19:00</span>
-          </td>
-          <td class="p-1">Workshops</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">20:00</span>
-          </td>
-          <td class="p-1"><span class="text-grey-dark">20:30</span> Aufräumen und Abschluss</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">21:00</span>
-          </td>
-          <td class="p-1">Ende des ersten Tages</td>
-        </tr>
-      </table>
+        <p class="font-bold mb-4 md:text-center text-xl">Tag 1</p>
+        <table>
+            <tr>
+                <td class="p-1"><span class="font-bold">09:00</span></td>
+                <td class="p-1">Ankommen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">10:00</span></td>
+                <td class="p-1">Kennenlernen & Erwartungen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">11:00</span></td>
+                <td class="p-1">Kreativübungen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">12:00</span></td>
+                <td class="p-1">Ideen finden und entwickeln</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">13:00</span></td>
+                <td class="p-1">Mittagessen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">14:00</span></td>
+                <td class="p-1">Pitch-Training und Ideen-Pitch</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">15:00</span></td>
+                <td class="p-1">Teamfindung</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">16:00</span></td>
+                <td class="p-1">Projekt-Kickoff</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">17:00</span></td>
+                <td class="p-1">Projektarbeit</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">18:00</span></td>
+                <td class="p-1">Abendessen</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">19:00</span></td>
+                <td class="p-1">Workshops</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">20:00</span></td>
+                <td class="p-1"><span class="text-grey-dark">20:30</span> Aufräumen und Abschluss</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">21:00</span></td>
+                <td class="p-1">Ende des ersten Tages</td>
+            </tr>
+        </table>
     </div>
     <div id="friday" class={{ $width }} p-4 mt-8 md:mt-0">
       <p class="font-bold mb-4 md:text-center text-xl">Tag 2</p>
-      <table>
-        <tr>
-          <td class="p-1 "><span class="font-bold">09:00</span>
-          </td>
-          <td class="p-1">Frühstück / <span class="text-grey-dark">9:30</span> Projektarbeit</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">10:00</span>
-          </td>
-          <td class="p-1">Projektarbeit</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">11:00</span>
-          </td>
-          <td class="p-1">Projektarbeit</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">12:00</span>
-          </td>
-          <td class="p-1">Workshops</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">13:00</span>
-          </td>
-          <td class="p-1">Mittagessen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">14:00</span>
-          </td>
-          <td class="p-1">Projektarbeit</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">15:00</span>
-          </td>
-          <td class="p-1">Projektarbeit</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">16:00</span>
-          </td>
-          <td class="p-1">Projektarbeit</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">17:00</span>
-          </td>
-          <td class="p-1">Workshops / Impulsvortrag</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">18:00</span>
-          </td>
-          <td class="p-1">Abendessen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">19:00</span>
-          </td>
-          <td class="p-1">Berufsorientierung</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">20:00</span>
-          </td>
-          <td class="p-1"><span class="text-grey-dark">20:30</span> Aufräumen und Abschluss</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">21:00</span>
-          </td>
-          <td class="p-1">Ende</td>
-        </tr>
-      </table>
+        <table>
+            <tr>
+                <td class="p-1 "><span class="font-bold">09:00</span></td>
+                <td class="p-1">Frühstück / <span class="text-grey-dark">9:30</span> Projektarbeit</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">10:00</span></td>
+                <td class="p-1">Projektarbeit</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">11:00</span></td>
+                <td class="p-1">Projektarbeit</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">12:00</span></td>
+                <td class="p-1">Workshops</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">13:00</span></td>
+                <td class="p-1">Mittagessen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">14:00</span></td>
+                <td class="p-1">Projektarbeit</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">15:00</span></td>
+                <td class="p-1">Projektarbeit</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">16:00</span></td>
+                <td class="p-1">Projektarbeit</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">17:00</span></td>
+                <td class="p-1">Workshops / Impulsvortrag</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">18:00</span></td>
+                <td class="p-1">Abendessen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">19:00</span></td>
+                <td class="p-1">Berufsorientierung</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">20:00</span></td>
+                <td class="p-1"><span class="text-grey-dark">20:30</span> Aufräumen und Abschluss</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">21:00</span></td>
+                <td class="p-1">Ende</td>
+            </tr>
+        </table>
     </div>
     <div id="saturday" class={{ $width }} p-4 mt-8 md:mt-0">
-      <p class="font-bold mb-4 md:text-center text-xl">Tag 3</p>
-      <table>
-        <tr>
-          <td class="p-1 "><span class="font-bold">09:00</span>
-          </td>
-          <td class="p-1">Frühstück / <span class="text-grey-dark">9:30</span> Projektarbeit</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">10:00</span>
-          </td>
-          <td class="p-1">Projektarbeit / Zwischenpräsentationen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">11:00</span>
-          </td>
-          <td class="p-1">Projektarbeit / Zwischenpräsentationen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">12:00</span>
-          </td>
-          <td class="p-1">Projektarbeit / Zwischenpräsentationen</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">13:00</span>
-          </td>
-          <td class="p-1">Mittagessen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">14:00</span>
-          </td>
-          <td class="p-1">Projektarbeit / Zwischenpräsentation</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">15:00</span>
-          </td>
-          <td class="p-1">Projektarbeit / Zwischenpräsentation</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">16:00</span>
-          </td>
-          <td class="p-1">Workshops</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">17:00</span>
-          </td>
-          <td class="p-1">Projektarbeit / Zwischenpräsentationen</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">18:00</span>
-          </td>
-          <td class="p-1">Abendessen</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">19:00</span>
-          </td>
-          <td class="p-1">Projektarbeit</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">20:00</span>
-          </td>
-          <td class="p-1">Entertainment-Abend</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">21:00</span>
-          </td>
-                    <td class="p-1">Entertainment-Abend <span class="text-grey-dark">(bis max. 24:00 Uhr)</td>
-        </tr>
-      </table>
+        <p class="font-bold mb-4 md:text-center text-xl">Tag 3</p>
+        <table>
+            <tr>
+                <td class="p-1 "><span class="font-bold">09:00</span></td>
+                <td class="p-1">Frühstück / <span class="text-grey-dark">9:30</span> Projektarbeit</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">10:00</span></td>
+                <td class="p-1">Projektarbeit / Zwischenpräsentationen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">11:00</span></td>
+                <td class="p-1">Projektarbeit / Zwischenpräsentationen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">12:00</span></td>
+                <td class="p-1">Projektarbeit / Zwischenpräsentationen</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">13:00</span></td>
+                <td class="p-1">Mittagessen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">14:00</span></td>
+                <td class="p-1">Projektarbeit / Zwischenpräsentation</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">15:00</span></td>
+                <td class="p-1">Projektarbeit / Zwischenpräsentation</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">16:00</span></td>
+                <td class="p-1">Workshops</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">17:00</span></td>
+                <td class="p-1">Projektarbeit / Zwischenpräsentationen</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">18:00</span></td>
+                <td class="p-1">Abendessen</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">19:00</span></td>
+                <td class="p-1">Projektarbeit</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">20:00</span></td>
+                <td class="p-1">Entertainment-Abend</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">21:00</span></td>
+                <td class="p-1">Entertainment-Abend <span class="text-grey-dark">(bis max. 24:00 Uhr)</td>
+            </tr>
+        </table>
     </div>
     <div id="sunday" class={{ $width }} p-4 mt-8 md:mt-0">
-      <p class="font-bold mb-4 md:text-center text-xl">Tag 4</p>
-      <table>
-        <tr>
-          <td class="p-1 "><span class="font-bold">09:00</span>
-          </td>
-          <td class="p-1">Frühstück / <span class="text-grey-dark">9:30</span> Projektarbeit</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">10:00</span>
-          </td>
-          <td class="p-1">Projektarbeit</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">11:00</span>
-          </td>
-          <td class="p-1">Projektarbeit / Camp-Verbesserungsworkshop</td>
-        </tr>
-        <tr class="bg-purple-lightest">
-          <td class="p-1"><span class="font-bold">12:00</span>
-          </td>
-          <td class="p-1">Mittagessen</td>
-        </tr>
-        <tr >
-          <td class="p-1"><span class="font-bold">13:00</span>
-          </td>
-          <td class="p-1">Abschlussrunde und Vorbereitung Abschlusspräsentation</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">14:00</span>
-          </td>
-          <td class="p-1">Abschlusspräsentation</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">15:00</span>
-          </td>
-          <td class="p-1">Abschlusspräsentation</td>
-        </tr>
-        <tr>
-          <td class="p-1"><span class="font-bold">16:00</span>
-          </td>
-          <td class="p-1">Ende</td>
-        </tr>
-      </table>
+        <p class="font-bold mb-4 md:text-center text-xl">Tag 4</p>
+        <table>
+            <tr>
+                <td class="p-1 "><span class="font-bold">09:00</span></td>
+                <td class="p-1">Frühstück / <span class="text-grey-dark">9:30</span> Projektarbeit</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">10:00</span></td>
+                <td class="p-1">Projektarbeit</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">11:00</span></td>
+                <td class="p-1">Projektarbeit / Camp-Verbesserungsworkshop</td>
+            </tr>
+            <tr class="bg-purple-lightest">
+                <td class="p-1"><span class="font-bold">12:00</span></td>
+                <td class="p-1">Mittagessen</td>
+            </tr>
+            <tr >
+                <td class="p-1"><span class="font-bold">13:00</span></td>
+                <td class="p-1">Abschlussrunde und Vorbereitung Abschlusspräsentation</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">14:00</span></td>
+                <td class="p-1">Abschlusspräsentation</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">15:00</span></td>
+                <td class="p-1">Abschlusspräsentation</td>
+            </tr>
+            <tr>
+                <td class="p-1"><span class="font-bold">16:00</span></td>
+                <td class="p-1">Ende</td>
+            </tr>
+        </table>
     </div>
-  </div> --}}
+</div> --}}
