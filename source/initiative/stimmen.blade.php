05ba78d1427cb5aa5828e80c8f77bc7f08874768
Julius <jubeki99@gmail.com>
2019-01-15 18:36:48 +0100

    Fix formatting initiative.stimmen

--- source/initiative/stimmen.blade.php
+++ source/initiative/stimmen.blade.php
@@ -5,61 +5,24 @@
 
     <div class="text-3xl mb-4 text-white">Stimmen zu Code+Design</div>
 
-   <div class="p-8 rounded bg-white">
+    <div class="p-8 rounded bg-white">
+        <div class="flex flex-wrap">
+            @foreach ($testimonials as $testimonial)
+                <div class="w-full md:w-1/2 p-1">
+                    @component('_components.card')
+                        @slot('src') {{ $testimonial->src }} @endslot
+                        @slot('link') {{ $testimonial->resource }} @endslot
+                        @slot('medium') {{ $testimonial->medium }} @endslot
+                        @slot('channel') {{ $testimonial->channel }} @endslot
+                        @slot('published_at') {{ date('d.m.y', $testimonial->published_at) }} @endslot
 
-    
-       
-    <div class="flex flex-wrap">
-        @foreach ($testimonials as $testimonial)
-                <div class="w-full md:w-1/2 p-1">@component('_components.card')
-                             
-                                             @slot('src')
-                                             {{ $testimonial->src }}
-                                             @endslot
-                             
-                                             @slot('link')
-                                             {{ $testimonial->resource }}
-                                             @endslot
-                             
-                                             @slot('medium')
-                                             {{ $testimonial->medium }}
-                                             @endslot
-                             
-                                             @slot('channel')
-                                            {{ $testimonial->channel }}
-                                             @endslot
-                             
-                                             @slot('published_at')
-                                             {{ date('d.m.y', $testimonial->published_at) }}
-                                             @endslot
-                             
-                                             @slot('ml')
-                                             @endslot
-                             
-                                             @slot('mr')
-                                             @endslot
-
-                                             @slot('m')
-                                             @endslot
-
-                                             @slot('mb')
-                                             @endslot
-
-                                             @yield('content')
-                             
-                                             {{ $testimonial->content }}
-                             
-                                          @endcomponent</div>         
-    
-        @endforeach
+                        @yield('content')
+                        {{ $testimonial->content }}
+                    @endcomponent
+                </div>
+            @endforeach
+        </div>
     </div>
-
-
-
-
-    </div>
-
-   </div>
 
 </main>
 
