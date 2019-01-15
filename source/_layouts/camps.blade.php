e5d0e381a37d9af6116224bc6556fe6e855ede8c
Julius <jubeki99@gmail.com>
2019-01-15 17:23:03 +0100

    Fix formatting _layouts.camps

--- source/_layouts/camps.blade.php
+++ source/_layouts/camps.blade.php
@@ -1,235 +1,143 @@
 @extends('_layouts.master')
 
 @section('body')
-
-
-
 <div class="p-8 bg-purple @if ($page->active === 'no') border-t-4 border-purple @endif">
-  @if ($page->active === 'no')
-<div class="text-purple-lighter">
-    Dieses {{ $page->type }} ist leider schon vorbei…
-</div>
-@endif
-   <h1 class="text-white">Komm' zum {{ $page->type }} in {{ $page->city }} 
-    @if ($page->days === 1) ({{ date('d.m.', $page->date_start) }}) @else 
-    ({{ date('d.m.', $page->date_start) }} bis {{ date('d.m.y', $page->date_end) }}) @endif</h1>
-
-
-<div class="md:flex mt-4">
-  <div class="md:w-2/3 bg-white rounded p-4">
-
-    <header>
-      @if ($page->youtube)
-          <div id='plyr-youtube' data-type="youtube" data-video-id="{{ $page->youtube }}"></div>
-        @else
-          <div><img src="/img/{{ $page->headerimage }}" alt=""></div>
-        @endif
-    </header>
-
-
-    @component('_components.camps.facts')
-        @slot('location')
-        {{ $page->location }}
-        @endslot
-
-        @slot('locationlink')
-        {{ $page->locationlink }}
-        @endslot
-
-        @slot('timestart')
-        {{ $page->time_start }}
-        @endslot
-
-        @slot('timeend')
-        {{ $page->time_end }}
-        @endslot
-
-        @slot('from')
-        {{ date('d.m.', $page->date_start) }}
-        @endslot
-
-        @slot('to')
-        {{ date('d.m.y', $page->date_end) }}
-        @endslot
-
-        @slot('days')
-        {{ $page->days }}
-        @endslot
-
-        @slot('cost')
-        {{ $page->cost }}
-        @endslot
-
-        @slot('costlaptop')
-        {{ $page->costlaptop }}
-        @endslot
-
-        @slot('meals')
-        {{ $page->meals }}
-        @endslot
-
-        @slot('type')
-        {{ $page->type }}
-        @endslot
-
-        @slot('laptopfree')
-        {{ $page->laptopfree }}
-        @endslot
-
-    @endcomponent
-
-@if ($page->teaser === 'yes')
-    @component('_components.camps.teaser')
-        @slot('location')
-        {{ $page->location }}
-        @endslot
-
-        @slot('from')
-        {{ date('d.m.y', $page->date_start) }}
-        @endslot
-
-        @slot('to')
-        {{ date('d.m.y', $page->date_end) }}
-        @endslot
-
-        @slot('city')
-        {{ $page->city }}
-        @endslot
-
-        @slot('holidays')
-        {{ $page->holidays }}
-        @endslot
-
-        @slot('days')
-        {{ $page->days }}
-        @endslot
-
-
-    @endcomponent
-@else
-  {!! $page->getContent() !!}
-{{--   @yield('content')--}}
-@endif
-
-    @if ($page->active === 'yes')
-    @include('_partials.campy', ['type' => $page->type, 'camp_id' => $page->camp_id ])
+    @if ($page->active === 'no')
+    <div class="text-purple-lighter">
+        Dieses {{ $page->type }} ist leider schon vorbei…
+    </div>
     @endif
-
-    @include('_partials.timetable', ['width' => 'w-1/2'])
-
-  </div>
-  <div class="md:w-1/3 bg-white rounded p-4 md:ml-4 mt-4 md:mt-0">
-    
-    <div class="mb-4">@if ($page->active === 'yes')
-    @include('_partials.campy', ['type' => $page->type, 'camp_id' => $page->camp_id ])
-    @endif</div>
-
-
-    @include('_partials.faq', ['cost' => $page->cost, 'costlaptop' => $page->costlaptop, 'campid' => $page->id])
-
-    @include('_partials.update')
+    <h1 class="text-white">
+        Komm' zum {{ $page->type }} in {{ $page->city }}
+        @if ($page->days === 1)
+            ({{ date('d.m.', $page->date_start) }})
+        @else
+            ({{ date('d.m.', $page->date_start) }} bis {{ date('d.m.y', $page->date_end) }})
+        @endif
+    </h1>
 
 
-  </div>
+    <div class="md:flex mt-4">
+        <div class="md:w-2/3 bg-white rounded p-4">
 
-</div>
+            <header>
+                @if ($page->youtube)
+                    <div id='plyr-youtube' data-type="youtube" data-video-id="{{ $page->youtube }}"></div>
+                @else
+                    <div><img src="/img/{{ $page->headerimage }}" alt=""></div>
+                @endif
+            </header>
 
-@if($page->supporters == 'yes')
-<div class="text-5xl mt-8 text-white font-bold uppercase font-sans tracking-wide">Förderer</div>
+            @component('_components.camps.facts')
+                @slot('location') {{ $page->location }} @endslot
+                @slot('locationlink') {{ $page->locationlink }} @endslot
+                @slot('timestart') {{ $page->time_start }} @endslot
+                @slot('timeend') {{ $page->time_end }} @endslot
+                @slot('from') {{ date('d.m.', $page->date_start) }} @endslot
+                @slot('to') {{ date('d.m.y', $page->date_end) }} @endslot
+                @slot('days') {{ $page->days }} @endslot
+                @slot('cost') {{ $page->cost }} @endslot
+                @slot('costlaptop') {{ $page->costlaptop }} @endslot
+                @slot('meals') {{ $page->meals }} @endslot
+                @slot('type') {{ $page->type }} @endslot
+                @slot('laptopfree') {{ $page->laptopfree }}@endslot
+            @endcomponent
 
-<div class="bg-white rounded p-8 mt-8">
-
-  <div class="flex flex-wrap ">
-            @foreach ($partners->filter->hasCamp($page->id) as $partner)
-
-            @if($partner->tier == 'silver' or $partner->tier == 'gold' or $partner->tier == 'bronze')
-                   <div class="p-4"><div class="flex flex-col items-center">
-                   
-                                   <div class="flex flex-col justify-center">
-                                   <a href="{{ $partner->website}}">
-                                    @component('_components.img')
-                                                                                                  
-                                      @slot('src')
-                                      /img/partner/{{ $partner->logo }}
-                                      @endslot
-                                  
-                                      @slot('alt')
-                                      {{ $partner->name }}
-                                      @endslot
-      
-                                      @slot('width')
-                                      {{ $partner->width }}
-                                      @endslot
-                                      
-                                      @slot('height')
-                                      {{ $partner->height ?? 'h-16'}}
-                                      @endslot
-                                                                      
-                                   @endcomponent
-                                  </a>
-                                   </div>
-
-                                   @if($page->long)
-                                   {{ $partner->long }}
-                                   @endif
-                                                                         
-                    </div>
-                </div>
+            @if ($page->teaser === 'yes')
+                @component('_components.camps.teaser')
+                    @slot('location') {{ $page->location }} @endslot
+                    @slot('from') {{ date('d.m.y', $page->date_start) }} @endslot
+                    @slot('to')  {{ date('d.m.y', $page->date_end) }}  @endslot
+                    @slot('city') {{ $page->city }} @endslot
+                    @slot('holidays') {{ $page->holidays }} @endslot
+                    @slot('days') {{ $page->days }} @endslot
+                @endcomponent
+            @else
+                {!! $page->getContent() !!}
             @endif
-            @endforeach
-</div></div>
-@endif
 
-@if($page->partners == 'yes')
-<div class="text-5xl mt-8 text-white font-bold uppercase font-sans tracking-wide">Partner</div>
+            @if ($page->active === 'yes')
+                @include('_partials.campy', ['type' => $page->type, 'camp_id' => $page->camp_id ])
+            @endif
 
-<div class="bg-white rounded p-8 mt-8">
+            @include('_partials.timetable', ['width' => 'w-1/2'])
 
-  <div class="flex flex-wrap ">
+        </div>
+        <div class="md:w-1/3 bg-white rounded p-4 md:ml-4 mt-4 md:mt-0">
+
+            <div class="mb-4">
+                @if ($page->active === 'yes')
+                    @include('_partials.campy', ['type' => $page->type, 'camp_id' => $page->camp_id ])
+                @endif
+            </div>
+
+            @include('_partials.faq', ['cost' => $page->cost, 'costlaptop' => $page->costlaptop, 'campid' => $page->id])
+
+            @include('_partials.update')
+        </div>
+
+    </div>
+
+    @if($page->supporters == 'yes')
+    <div class="text-5xl mt-8 text-white font-bold uppercase font-sans tracking-wide">Förderer</div>
+    <div class="bg-white rounded p-8 mt-8">
+        <div class="flex flex-wrap ">
             @foreach ($partners->filter->hasCamp($page->id) as $partner)
-                   @if ($partner->tier == 'patron' or $partner->tier == 'partner' )
-                   <div class="p-4"><div class="flex flex-col items-center">
-                   
-                                   <div class="h-32 flex flex-col justify-center">
-                                   <a href="{{ $partner->website}}">
+                @if($partner->tier == 'silver' or $partner->tier == 'gold' or $partner->tier == 'bronze')
+                    <div class="p-4">
+                        <div class="flex flex-col items-center">
+                            <div class="flex flex-col justify-center">
+                                <a href="{{ $partner->website}}">
                                     @component('_components.img')
-                                                                                                  
-                                      @slot('src')
-                                      /img/partner/{{ $partner->logo }}
-                                      @endslot
-                                  
-                                      @slot('alt')
-                                      {{ $partner->name }}
-                                      @endslot
-      
-                                      @slot('width')
-                                      {{ $partner->width }}
-                                      @endslot
-                                      
-                                      @slot('height')
-                                      {{ $partner->height or "h-16"}}
-                                      @endslot
-                                                                      
-                                   @endcomponent
-                                  </a>
-                                   </div>
-
-                                   @if($page->long)
-                                   {{ $partner->long }}
-                                   @endif
-                                                                         
+                                        @slot('src') /img/partner/{{ $partner->logo }} @endslot
+                                        @slot('alt') {{ $partner->name }} @endslot
+                                        @slot('width') {{ $partner->width }} @endslot
+                                        @slot('height') {{ $partner->height ?? 'h-16'}} @endslot
+                                    @endcomponent
+                                </a>
+                            </div>
+                            @if($page->long)
+                            {{ $partner->long }}
+                            @endif
+                        </div>
                     </div>
-                </div>
                 @endif
             @endforeach
-</div></div>
-@endif
+        </div>
+    </div>
+    @endif
+
+    @if($page->partners == 'yes')
+    <div class="text-5xl mt-8 text-white font-bold uppercase font-sans tracking-wide">Partner</div>
+    <div class="bg-white rounded p-8 mt-8">
+        <div class="flex flex-wrap ">
+            @foreach ($partners->filter->hasCamp($page->id) as $partner)
+                @if ($partner->tier == 'patron' or $partner->tier == 'partner' )
+                    <div class="p-4">
+                        <div class="flex flex-col items-center">
+                            <div class="h-32 flex flex-col justify-center">
+                                <a href="{{ $partner->website}}">
+                                    @component('_components.img')
+                                        @slot('src') /img/partner/{{ $partner->logo }} @endslot
+                                        @slot('alt') {{ $partner->name }} @endslot
+                                        @slot('width') {{ $partner->width }} @endslot
+                                        @slot('height') {{ $partner->height or "h-16"}} @endslot
+                                    @endcomponent
+                                </a>
+                            </div>
+                            @if($page->long)
+                            {{ $partner->long }}
+                            @endif
+
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+        </div>
+    </div>
+    @endif
 
 @endsection
-
-
-
-
 
 @section('meta')
 
