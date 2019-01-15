e9536fba2b811f79d7c676454c65b2e35ba0a56e
Julius <jubeki99@gmail.com>
2019-01-15 18:47:20 +0100

    Fix formatting coaches

--- source/coaches.blade.php
+++ source/coaches.blade.php
@@ -8,32 +8,16 @@
             @foreach ($coaches as $coach)
                 <div class="w-full md:w-1/2 p-1">
                     @component('_components.person_card')
-                         @slot('image')
-                            {{ $coach->image }}
-                         @endslot
-                         @slot('section')
-                            {{ $coach->section }}
-                         @endslot
-                         @slot('firstname')
-                            {{ $coach->firstname }}
-                         @endslot
-                         @slot('lastname')
-                            {{ $coach->lastname }}
-                         @endslot
-                         @slot('bio')
-                            {{ $coach->bio }}
-                         @endslot
-                         @slot('website')
-                            {{ $coach->website }}
-                         @endslot
-                         @slot('github')
-                            {{ $coach->github }}
-                         @endslot
-                         @slot('twitter')
-                            {{ $coach->twitter }}
-                         @endslot
-                     @endcomponent
-                  </div>
+                        @slot('image') {{ $coach->image }} @endslot
+                        @slot('section') {{ $coach->section }} @endslot
+                        @slot('firstname') {{ $coach->firstname }} @endslot
+                        @slot('lastname') {{ $coach->lastname }} @endslot
+                        @slot('bio') {{ $coach->bio }} @endslot
+                        @slot('website') {{ $coach->website }} @endslot
+                        @slot('github') {{ $coach->github }} @endslot
+                        @slot('twitter') {{ $coach->twitter }} @endslot
+                    @endcomponent
+                </div>
             @endforeach
         </div>
     </div>
