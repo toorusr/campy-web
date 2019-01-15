9a918fd846db2b482b7592fabcb7741ec6ec51b0
Julius <jubeki99@gmail.com>
2019-01-15 18:47:55 +0100

    Fix formatting dabei

--- source/dabei.blade.php
+++ source/dabei.blade.php
@@ -6,10 +6,9 @@
         <p class="text-2xl mb-4">Danke für deine Anmeldung!</p>
         <ul class="text-lg leading-loose">
            <li>Wir freuen uns, dass du dabei sein wirst! Wir schreiben dir so schnell wie möglich per SMS, wie du deine Anmeldung bestätigst.</li>
-           <li class="text-sm">Keine SMS bekommen und 24h durch? Bitte erinnere uns <a href="mailto:hello@code.design?subject=Erinnerung%20Anmeldung&amp;body=Hallo,%20ich%habe%20mich%20angemeldet%20und%20warte%20auf%20eine%20Ant">kurz per Mail :)</a>
-        <li class="font-bold"></li> 
+           <li class="text-sm">Keine SMS bekommen und 24h durch? Bitte erinnere uns <a href="mailto:hello@code.design?subject=Erinnerung%20Anmeldung&amp;body=Hallo,%20ich%habe%20mich%20angemeldet%20und%20warte%20auf%20eine%20Ant">kurz per Mail :)</a></li>
         </ul>
-        
+
     </div>
 </div>
 @endsection
