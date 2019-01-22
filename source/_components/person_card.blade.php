b66f48708ec15cd0838158c18ac4c6c311a0e456
Julius <jubeki99@gmail.com>
2019-01-22 10:24:44 +0100

    Fix Padding, Margin and word breaking

--- source/_components/person_card.blade.php
+++ source/_components/person_card.blade.php
@@ -1,4 +1,4 @@
-<div class="flex-1 rounded border px-6 py-4 max-w-md h-full {{ $ml ?? '' }} {{ $mr ?? '' }} m-{{ $m ?? '' }} mb-{{ $mb ?? '' }}">
+<div class="flex-1 rounded border p-3 md:px-6 md:py-4 max-w-md h-full {{ $ml ?? '' }} {{ $mr ?? '' }} {{ $m ?? '' }} { $mb ?? '' }}">
     <div class="flex items-center">
         <img src="{{ $image ?? ''}}" class="h-12 w-12 rounded-full" />
         <div class="flex flex-col ml-4">
@@ -6,10 +6,10 @@
             <span class="text-grey"> {{ $section ?? ''}} </span>
         </div>
     </div>
-    <div class=" mt-3 mb-1 leading-normal text-lg">
-        <ul class="text-center">
+    <div class=" mt-3 leading-normal text-lg">
+        <div class="text-center">
             @if ($bio != '')
-                <p class="text-grey-darker"> {{ $bio ?? ''}} </p>
+                <p class="text-grey-darker break-words"> {{ $bio ?? ''}} </p>
             @endif
             @if ($github != '')
                 <a href="https://github.com/{{$github ?? ''}}"><img src="/img/github.svg" width="24" height="24" /></a>
@@ -20,6 +20,6 @@
             @if ($website != '')
                 <a href="{{$website ?? ''}}"><img src="/img/external-link.svg" width="24" height="24" /></a>
             @endif
-        </ul>
+        </div>
     </div>
 </div>
